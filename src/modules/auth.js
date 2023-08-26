61047d250d3e3ccbd63c604dccbc43cc55d15a12
Alisher <alisherkarimov00007@gmail.com>
2023-08-26 11:20:48 +0500

    config

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,7 +1,40 @@
+import AuthServise from "../service/auth";
 
+const state = {
+  isLoading: false,
+  isLoggedIn: false,
+};
 
-const state = {}
+const modules = {
+  authStart(state) {
+    state.isLoading = true;
+  },
+  authSuccess(state) {
+    state.isLoading = false;
+    state.isLoggedIn = true;
+  },
+  authFailure(state) {
+    state.isLoading = false;
+    state.isLoggedIn = false;
+  },
+};
 
-const modules = {}
+const actions = {
+  register(context, user) {
+    return new Promise((resolve, reject) => {
+      context.commit("authStart");
+      AuthServise.register(user)
+        .then((res) => {
+          console.log(res);
+          context.commit("authSuccess");
+        })
+        .err((err) => {
+          context.commit("authFailure");
 
-const actions = {}
+          console.log(err);
+        });
+    });
+  },
+};
+
+export default { state, modules, actions };
